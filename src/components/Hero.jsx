36b38950c4df07bc42311d80ff45ae029a7222ba
prabin.bhusal <prabin.bhusal@tai.com.np>
2025-12-01 14:34:22 +0545

    height fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ import { pm } from "../assets";
 
 const Hero = () => {
   return (
-    <section className="relative w-full mx-auto">
+    <section className="relative w-full h-screen mx-auto">
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
